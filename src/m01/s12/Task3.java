8601558c170f0b7dcf62b699d488b693216b806c
mobile <mobile@itcube.win>
2020-09-17 20:22:40 +0500

    mobile

---
+++ src/m01/s12/Task3.java
@@ -0,0 +1,10 @@
+package tasks.src.m01.s12;
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(a + 2 - a % 2);
+    }
+}
